2a0c67093b69ce37440ce7959699d7a4048e8bf8
ngay <nathanaelwgay@netscape.net>
2019-08-02 09:51:40 -0400

    Created location data set.

---
+++ R/locations.R
@@ -0,0 +1,14 @@
+#' Keiser QEP Locations
+#'
+#' An updated list of full Keiser campus names, and associated campus abbreviations and QEP starts.
+#'
+#' \itemize{
+#' \item full Full Keiser University location name.
+#' \item abbreviation Abbreviated Keiser location.
+#' \item start_term Term code when that campus began the QEP.
+#' }
+#'
+#' @format A data frame with 8 rows and 3 columns.
+#' @name locations
+#'
+NULL
