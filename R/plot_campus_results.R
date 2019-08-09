2b7775ab94264d3886fca5cd80762c861853ecbf
ngay <nathanaelwgay@netscape.net>
2019-08-09 09:13:29 -0400

    Update function to export ggplot object.

--- R/plot_campus_results.R
+++ R/plot_campus_results.R
@@ -2,13 +2,14 @@
 #'
 #' @param data The internal master grade dataframe for the report.
 #' @param location The campus for which the plot is generated.
+#' @return campus_plot A ggplot object.
 #' @export plot_campus_results
 
 plot_campus_results <- function(data,location){
   plot_data <- data %>%
     group_by(termcode_new,Campus) %>%
     summarize(size=n(),pro_suc=sum(Success=="Successful")/size)
-  ggplot(plot_data, mapping=aes(x=termcode_new,y=pro_suc,group=1)) +
+  campus_plot <- ggplot(plot_data, mapping=aes(x=termcode_new,y=pro_suc,group=1)) +
     geom_line() +
     geom_point() +
     ylim(0, 1) +
